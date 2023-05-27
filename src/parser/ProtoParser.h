9e39929e9b847998a0f5603cf834bd5972f7896c
Nagy Levente <woundless17@gmail.com>
2023-05-27 10:49:23 +0200

    Added dummy Test module with google-test

--- src/parser/ProtoParser.h
+++ src/parser/ProtoParser.h
@@ -1,7 +1,18 @@
 #ifndef EFFECTIVEBROCCOLI_PROTOPARSER_H
 #define EFFECTIVEBROCCOLI_PROTOPARSER_H
 
+#include <string>
+
+enum class ProtoParamType {
+    STRING,
+    INTEGER,
+    DOUBLE
+};
+
+/// @brief Wrapper around the generated serialization classes
 class ProtoParser {
+public:
+    ProtoParser();
 
 };
 
